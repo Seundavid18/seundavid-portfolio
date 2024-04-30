517576ff207ede02bf18c50a4f88961a89d2bfad
Seundavid18 <oyewumidavid84@gmail.com>
2024-04-30 16:36:07 +0100

    Added Azure logos

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -14,7 +14,9 @@ import {IoLogoJavascript} from 'react-icons/io'
 import {GrReactjs} from 'react-icons/gr'
 import {DiNodejsSmall} from 'react-icons/di'
 import {SiMongodb} from 'react-icons/si'
-
+import { SiMicrosoftazure } from "react-icons/si";
+import { SiAzuredevops } from "react-icons/si";
+import { SiAzurepipelines } from "react-icons/si";
 
 const Portfolio = () => {
     return(
@@ -175,7 +177,10 @@ const Portfolio = () => {
                                     <div><FaBootstrap className="me-3" color="#563d7c" size={20}/></div>
                                     <div><IoLogoJavascript className="me-3" color=" #f0db4f" size={20}/></div>
                                     <div><DiNodejsSmall className="me-3" color=" #88b04a" size={20}/></div>
-                                    <div><SiMongodb color=" #3e912f" size={20}/></div>
+                                    <div><SiMongodb color=" #3e912f" className="me-3" size={20}/></div>
+                                    <div><SiMicrosoftazure color=" #0080FF" className="me-3" size={17}/></div>
+                                    <div><SiAzuredevops color=" #007FFF" className="me-3" size={17}/></div>
+                                    <div><SiAzurepipelines color=" #007FFF" size={17}/></div>
                                 </div>
                                 <a class="hire-me" href="https://seundavidblog.azurewebsites.net/" target="blank">Visit site</a>
                             </div>
@@ -194,13 +199,16 @@ const Portfolio = () => {
                         <div className="about-flex">
                             <div className="container">
                                 <h4 className="fs-4 fw-bold pt-5 pb-3">A Bank Web Application</h4>
-                                <h6 className="portfolio-font pb-2">This is a web application for carrying out deposit, transfer and bills transactions</h6>
+                                <h6 className="portfolio-font pb-2">This is a web application for carrying out deposit, transfer and bills transactions.</h6>
                                 <div style={{display: "flex", flexDirection: "row", justifyCcontent: "center", paddingTop: "5px", paddingBottom: '45px'}}>
                                     <div><GrReactjs className="me-3" color="#61dbfb" size={20}/></div>
                                     <div><FaBootstrap className="me-3" color="#563d7c" size={20}/></div>
                                     <div><IoLogoJavascript className="me-3" color=" #f0db4f" size={20}/></div>
                                     <div><DiNodejsSmall className="me-3" color=" #88b04a" size={20}/></div>
-                                    <div><SiMongodb color=" #3e912f" size={20}/></div>
+                                    <div><SiMongodb color=" #3e912f" className="me-3" size={20}/></div>
+                                    <div><SiMicrosoftazure color=" #0080FF" className="me-3" size={17}/></div>
+                                    <div><SiAzuredevops color=" #007FFF" className="me-3" size={17}/></div>
+                                    <div><SiAzurepipelines color=" #007FFF" size={17}/></div>
                                 </div>
                                 <a class="hire-me" href="https://seunbankapp.azurewebsites.net/" target="blank">Visit site</a>
                             </div>

728dd0e73c4e36ee03a2be6dc948814fc6dec7e5
Seundavid18 <oyewumidavid84@gmail.com>
2024-07-31 18:19:08 +0100

    updated font for hero page

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
                 <div className="container">
                     <div className="hero-padding">
                         <div className="text-white text-center">
-                            <h1 className="pb-2 comp">SEUN DAVID DEV</h1>
+                            <h1 className="pb-2 fw-bold">SEUN DAVID DEV</h1>
                             <h6 className="pb-5">React js | Node.js | Microsoft Azure | Azure DevOps | MongoDB | Cloud Engineer<br /> If you're a business seeking a web presence or are looking <br /> to hire, contact me here</h6>
                             <div className="d-grid justify-content-center gap-4 d-lg-flex pt-4">
                                 <button class="btn btn-light1" type="button"><a href="#contact" style={{textDecoration: 'none', color: '#400c7b'}}>Hire Me<span className="ps-3"><BsEmojiSmile size={20} /></span></a></button>
